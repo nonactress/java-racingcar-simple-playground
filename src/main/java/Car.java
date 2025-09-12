60bd9f3b4fb52e0bbff78cd5df6ca865ee440b42
nonactress <shjshj1007@daum.net>
2025-09-12 13:25:48 +0900

    Git -commit feat: Car 객체 들여쓰기 수정 및 하드코딩 수정

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,14 +4,19 @@ public class Car {
     private final String name;
     private int speed;
     private int speedCnt;
-
+    private final int MINIMUM_SPEED_TO_MOVE = 4;
+    java.util.Random random = new java.util.Random();
 
     public Car(String name) {
         this.name = name;
-        this.speedCnt=0;
+        this.speedCnt = 0;
     }
 
     public int getSpeedCnt() {
+
+        if (speedCnt < Integer.MAX_VALUE) {
+            speedCnt++;
+        }
         return speedCnt;
     }
 
@@ -24,13 +29,12 @@ public class Car {
     }
 
     private void setSpeed() {
-        java.util.Random random = new java.util.Random();
         this.speed = random.nextInt(10);
     }
 
     public void carMove() {
         setSpeed();
-        if (speed >= 4) {
+        if (speed >= MINIMUM_SPEED_TO_MOVE) {
             upSpeedCnt();
         }
     }
