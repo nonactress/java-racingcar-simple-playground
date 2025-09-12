a9af148f06b121dd0f34f61a0b141dbcebe103e9
nonactress <tjgsuwls1007@gmail.com>
2025-09-12 20:34:07 +0900

    Git -commit feat: RaceSetting 클래스 생성

---
+++ src/main/java/RaceSetting.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class RaceSetting {
+    private Car[] cars;
+    private int carNum;
+    private int raceNum;
+    int[] forwardCnt;
+
+    public void setCarNum(Scanner scanner)
+    {
+        System.out.println("출전한 자동차 수: ");
+        this.carNum = scanner.nextInt();
+    }
+
+    private void setRaceNum(Scanner scanner) {
+        System.out.println("몇 번 달릴꺼야: ");
+        this.raceNum = scanner.nextInt();
+    }
+
+    public void setCars(Scanner scanner) {
+        this.cars=new Car[carNum];
+
+        for(int i=0;i<raceNum;i++)
+        {
+            String temp=scanner.next();
+            cars[i].setName(temp);
+        }
+    }
+
+    public void setRace()
+    {
+        Scanner scanner = new Scanner(System.in);
+        setCarNum(scanner);
+        setRaceNum(scanner);
+        setCars(scanner);
+        scanner.close();
+    }
+}
