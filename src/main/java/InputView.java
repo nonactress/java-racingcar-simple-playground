035ecdd517d4849c7ef3de41e11bd56dfb8f9a41
nonactress <tjgsuwls1007@gmail.com>
2025-09-15 10:14:52 +0900

    feat : mvc 패턴 적용 - 새로운 클래스 'InputView'

---
+++ src/main/java/InputView.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String[] getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return scanner.nextLine().split(",");
+    }
+
+    public static int getRaceCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = scanner.nextInt();
+        scanner.nextLine(); // 개행 문자 제거
+        return count;
+    }
+}
