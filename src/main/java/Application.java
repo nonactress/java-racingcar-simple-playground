7f5f4290abab5a626a3bb892525efab4dde4f8b8
nonactress <tjgsuwls1007@gmail.com>
2025-09-15 11:04:51 +0900

    feat : mvc 패턴 적용 - 새로운 클래스 'Application'

---
+++ src/main/java/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+    public static void main(String[] args) {
+        RacingController racingController = new RacingController();
+        racingController.run();
+    }
+}
