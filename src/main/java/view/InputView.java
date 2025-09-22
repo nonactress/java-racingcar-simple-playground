f6c6e25d7bdfea7aaa55f8e8b86678c1e325bb00
nonactress <tjgsuwls1007@gmail.com>
2025-09-22 14:09:45 +0900

    refactor (view) :apply 'try-catch' rogic

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,19 +6,26 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String[] getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] splitCar = scanner.nextLine().split(",");
-        while(true)
-        {
-            String[] splits = getStrings(splitCar);
-            if (splits != null) return splits;
-            splitCar = scanner.nextLine().split(",", -1);
-        }
+        String[] splits = null;
+        boolean splitsCheck = false;
+        while (true) {
+            try {
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+                splits = scanner.nextLine().split(",", -1);
 
+                // 예외가 발생할 수 있는 검증 메서드를 호출
+                splitsCheck = isValid(splits);
+            } catch (IllegalArgumentException e) {
+                // validateCarNames에서 예외를 던지면 이 코드가 실행됨
+                // 예외에 담긴 메시지를 출력하고, while 루프의 처음으로 돌아감
+                System.out.println(e.getMessage());
+            }
+            if (splits != null && splitsCheck) return splits;
+        }
     }
 
     private static String[] getStrings(String[] splitCar) {
-        if(isValid(splitCar)){
+        if (isValid(splitCar)) {
             return splitCar;
         }
         return null;
@@ -31,15 +38,14 @@ public class InputView {
         return count;
     }
 
-    public static boolean isValid(String [] cars){
-        if(cars.length == 1 ){
+    public static boolean isValid(String[] cars) {
+        if (cars.length == 1) {
             System.out.println("한 개 이상의 자동차를 입력해주세요!");
             return false;
         }
 
         for (String car : cars) {
-            if(car.trim().isEmpty())
-            {
+            if (car.trim().isEmpty()) {
                 System.out.println("자동차의 이름이 공백일 순 없습니다!");
                 return false;
             }
